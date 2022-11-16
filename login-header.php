01636523d81a652b56cd90768c1bbb1af862edad
chiloanerk <chiloane.rk@gmail.com>
2022-11-16 22:03:30 +0200

    Login Page

--- login-header.php
+++ login-header.php
@@ -7,10 +7,14 @@
 <nav class="col-sm-2">
     <div class="btn-group-vertical btn-group-sm" role="group" aria-label="Button Group">
         <button type="button" class="btn btn-secondary" onclick="location.href = 'login.php'">
-            Login
+            Erase Entries
         </button>
         <button type="button" class="btn btn-secondary" onclick="location.href = 'register-page.php'">
             Register
         </button>
+        <button type="button" class="btn btn-secondary" onclick="location.href = 'index.php'">
+            Cancel
+        </button>
+
     </div>
 </nav>
---
+++ login.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template for an interactive web page</title>
+    <!-- Bootstrap CSS File -->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous">
+    <script src="verify.js"></script>
+</head>
+<body>
+<div class="container" style="margin-top: 30px">
+    <!-- Header Section -->
+    <header class="jumbotron text-center row col-sm-14"
+            style="margin-bottom: 2px;
+            background: linear-gradient(white, #0073e6);
+            padding: 20px;">
+        <?php include('login-header.php'); ?>
+    </header>
+<!-- Body Section   -->
+    <div class="row" style="padding-left: 0;">
+<!--        Left-side Column Menu Section-->
+        <nav class="col-sm-2">
+            <ul class="nav nav-pills flex-column">
+                <?php include('nav.php'); ?>
+            </ul>
+        </nav>
+<!--        Validate Input-->
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            require ('process-login.php');
+        } // End of the main Submit conditional.
+        ?>
+        <div class="col-sm-8">
+            <h2 class="h2 text-center">Login</h2>
+            <form action="login.php" method="post" name="loginform" id="loginform">
+                <div class="form-group row">
+                    <label for="email" class="col-sm-4 col-form-label">Email Address:</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="email" name="email"
+                        placeholder="Email" maxlength="30" required
+                        value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-4 col-form-label">Password:</label>
+                    <div class="col-sm-8">
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Password" maxlength="40" required
+                               value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>">
+                        <p><span>Between 8 and 12 characters.</span></p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <input id="submit" class="btn btn-primary" type="submit" name="submit" value="Login">
+                    </div>
+                </div>
+            </form>
+        </div>
+<!--        Right-side Column Content Section-->
+        <?php
+        if (!isset($errorstring)) {
+            echo "<aside class'col-sm-2'>";
+            include ('info-col.php');
+            echo '</aside>';
+            echo '</div>';
+            echo '<footer class="jumbotron text-center row col-sm-14"
+                  style="padding-bottom:1px; padding-top:8px;">';
+        } else {
+            echo "<footer class='jumbotron text-center col-sm-12'
+                  style='padding-bottom:1px; padding-top:8px;'>";
+        }
+        include ('footer.php');
+        ?>
+        </footer>
+    </div>
+</div>
+</body>
+</html>
